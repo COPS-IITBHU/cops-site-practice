ba649d31a27029f7ae98355d479bfc6a7a683d66
kkvanonymous <ninja.kunal01@gmail.com>
2020-10-15 10:14:14 +0530

    Add vue-particles componenet

---
+++ components/Vue-particles/README.md
@@ -0,0 +1,6 @@
+This file consist of various color codes for the *vue-particles* component
+
+# Color Codes
+- ['#ef4cef','#4d395c','#212121'] Purple+Grey
+- ['#4CB1EF','#424959','#212121'] Blue+Grey
+- ['#984998','#332a41','#000000'] Purple+Black
---
+++ components/Vue-particles/vue-particles.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+    <div class="bg">
+        <canvas id="background"></canvas>
+   </div>
+</div>
+    
+</template>
+
+<script>
+import Thpace from 'thpace';
+export default {
+    name:'vue-particles',
+    mounted(){
+        const canvas = document.getElementById('background');
+        const settings = {
+            colors: ['#984998', '#332a41', '#000000'],
+            triangleSize: 80,
+            particleSettings:{
+                count:5,
+                radius:1
+            }
+        };
+
+        Thpace.create(canvas, settings);
+    }
+    
+}
+</script>
+
+<style scoped>
+html,
+body {
+    margin: 0; 
+}
+.bg {
+    height: 50vh;
+}
+</style>
