53237505531b011089f73373448ded8833b9eaf6
m-e-l-u-h-a-n <pururshottam.tiwari.cd.cse19@itbhu.ac.in>
2020-10-19 02:19:14 +0530

    Implemented graphics-cards components required for navigation.
    A gc-try.vue file has been included in pages directory for testing the cards.

---
+++ pages/gc-try.vue
@@ -0,0 +1,69 @@
+<template>
+  <div flat class="demo-display">
+    <v-row justify="space-around">
+      <v-col sm="6">
+        <graphic-nav-card
+          :image-link="imageLink"
+          theme-color="light-green darken-1"
+          :title="title"
+          :subtitle="subtitle"
+          nav-link="#"
+          class="graphic-card"
+        >
+        </graphic-nav-card>
+      </v-col>
+      <v-col sm="6">
+        <graphic-nav-card
+          :image-link="imageLink"
+          theme-color="light-green darken-1"
+          :title="title"
+          :subtitle="subtitle"
+          nav-link="#"
+          class="graphic-card"
+        >
+        </graphic-nav-card>
+      </v-col>
+    </v-row>
+    <v-row justify="space-around">
+      <v-col sm="6">
+        <graphic-nav-card
+          :image-link="imageLink"
+          theme-color="light-green darken-1"
+          :title="title"
+          :subtitle="subtitle"
+          nav-link="#"
+          class="graphic-card"
+        >
+        </graphic-nav-card>
+      </v-col>
+      <v-col sm="6">
+        <graphic-nav-card
+          :image-link="imageLink"
+          theme-color="light-green darken-1"
+          :title="title"
+          :subtitle="subtitle"
+          nav-link="#"
+          class="graphic-card"
+        >
+        </graphic-nav-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+<script>
+import GraphicNavCard from '@/components/GraphicCardNav'
+export default {
+  components: {
+    GraphicNavCard,
+  },
+  data() {
+    return {
+      imageLink:
+        'https://content-api-us1.codeavengers.com/file/cloud/assets/jr-01.png',
+      title: "The Dev's",
+      subtitle: 'IIT (BHU) development hub.',
+    }
+  },
+}
+</script>
+<style scoped lang="scss"></style>
