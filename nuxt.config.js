11df78eb40ec5ff3f21f8910fd2caa722afcd95a
Aryaman Gupta <aryaman.iitbhu@gmail.com>
2020-12-02 02:18:25 +0530

    first

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,7 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
 
+  
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     titleTemplate: '%s - cops-site',
@@ -13,14 +14,17 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }, {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Ubuntu'
+    },{rel:"stylesheet", href:"https://fonts.googleapis.com/css2?family=Dosis:wght@500&family=Neucha&family=Raleway:wght@500&display=swap"}],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
+  plugins: [{ src: "@/plugins/aos", ssr: false }],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -52,6 +56,7 @@ export default {
   // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
+    treeShake: true,
     theme: {
       dark: true,
       themes: {
---
+++ pages/csoc/timeline.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <CommonTimeline :items="items" />
+  </div>
+</template>
+
+<script>
+import CommonTimeline from '@/components/CommonTimeline'
+
+export default {
+  components: {
+    CommonTimeline,
+  },
+  async fetch() {
+    const data = await this.$content('timeline_data').fetch()
+    this.items = data.items
+  },
+  data() {
+    return {
+      items: [],
+    }
+  },
+}
+</script>
