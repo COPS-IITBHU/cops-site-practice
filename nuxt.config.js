531c170c5d8a47ca7c5f8fcbd8b3b0a7ec17b140
nb9960 <nbodani8@gmail.com>
2020-12-05 21:19:01 +0530

    minor change

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,9 +20,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-    {src: '@/plugins/youtube', ssr:false}
-  ],
+  plugins: [{ src: '@/plugins/youtube', ssr: false }],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
