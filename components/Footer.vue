dc1fb93166a55a236bd1e5a9bd11499d0ee608a7
Lakshya Singh <lakshay.singh1108@gmail.com>
2020-12-23 21:26:48 +0530

    fix: footer responsive
    row followed by cols for for proper use

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,27 +1,43 @@
 <template>
   <v-footer>
-    <v-row justify="center" align="center" class="d-flex flex-wrap">
-      <div class="pr-4">
+    <v-row justify="center" align="center">
+      <v-col cols="3" lg="2" class="pr-4 text-center">
         Copyright &copy; {{ new Date().getFullYear() }} COPS
-      </div>
-      <div
+      </v-col>
+      <v-col
+        cols="3"
+        lg="2"
         class="px-4"
         style="border: solid #fff; border-width: 0 0.1em 0 0.1em"
       >
-        <img
-          src="~/assets/images/new-cops-logo.jpg"
-          width="75px"
-          height="75px"
+        <v-img
+          class="mx-auto"
+          :src="logo"
+          alt="COPS Logo"
+          contain
+          max-width="110"
         />
-      </div>
-      <div class="ma-2">
+      </v-col>
+      <v-col cols="3" lg="2" class="white--text text-center">
         <a
           target="_blank"
+          aria-label="Code of Conduct"
           href="https://github.com/COPS-IITBHU/Code-Of-Conduct/blob/master/README.md"
-          style="text-decoration-line: none; color: #fff"
+          style="text-decoration-line: none"
           >Code of Conduct</a
         >
-      </div>
+      </v-col>
     </v-row>
   </v-footer>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      logo: require('../assets/images/new-cops-logo.jpg'),
+    }
+  },
+})
+</script>
