466cbf57abc4819faac603940004db701cee7010
Lakshya Singh <lakshay.singh1108@gmail.com>
2021-05-19 16:30:59 +0530

    refactor: change footer to normal html
    use tailwind css classes with ! if required
    semantic html replacement for vuetify components
    vue3 setup used
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,43 +1,81 @@
 <template>
-  <v-footer>
-    <v-row justify="center" align="center">
-      <v-col cols="3" lg="2" class="pr-4 text-center">
-        Copyright &copy; {{ new Date().getFullYear() }} COPS
-      </v-col>
-      <v-col
-        cols="3"
-        lg="2"
-        class="px-4"
-        style="border: solid #fff; border-width: 0 0.1em 0 0.1em"
+  <footer class="tw-flex tw-justify-evenly tw-p-4 tw-container tw-mx-auto">
+    <div>
+      <img
+        class="tw-mx-auto tw-object-contain"
+        :src="logo"
+        alt="Club of Programmers IIT BHU Logo"
+        width="90"
+      />
+    </div>
+    <address class="tw-flex tw-flex-col tw-justify-center">
+      <span>Copyright &copy; {{ new Date().getFullYear() }} COPS</span>
+      <a
+        target="_blank"
+        aria-label="Code of Conduct"
+        href="https://github.com/COPS-IITBHU/Code-Of-Conduct/blob/master/README.md"
+        style="text-decoration-line: none"
       >
-        <v-img
-          class="mx-auto"
-          :src="logo"
-          alt="COPS Logo"
-          contain
-          max-width="110"
-        />
-      </v-col>
-      <v-col cols="3" lg="2" class="white--text text-center">
-        <a
-          target="_blank"
-          aria-label="Code of Conduct"
-          href="https://github.com/COPS-IITBHU/Code-Of-Conduct/blob/master/README.md"
-          style="text-decoration-line: none"
-        >
-          Code of Conduct
-        </a>
-      </v-col>
-    </v-row>
-  </v-footer>
+        Code of Conduct
+      </a>
+      <ul
+        class="
+          tw-flex tw-list-none tw-justify-between tw-py-2 tw-w-full
+          !tw-pl-0
+        "
+      >
+        <li v-for="icon in socialMedia" :key="icon.url">
+          <a
+            :href="icon.url"
+            target="_blank"
+            rel="noreferrer"
+            :aria-label="icon.content"
+          >
+            <span
+              class="
+                iconify
+                tw-w-6 tw-h-6
+                hover:tw-scale-110
+                tw-transform tw-cursor-pointer
+              "
+              :data-icon="icon.icon"
+            ></span>
+          </a>
+        </li>
+      </ul>
+    </address>
+  </footer>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({
-  data() {
+import { defineComponent } from '@vue/composition-api'
+export default defineComponent({
+  setup() {
+    const socialMedia = [
+      {
+        icon: 'mdi:github',
+        url: 'https://github.com/COPS-IITBHU',
+        content: 'Github Org',
+      },
+      {
+        icon: 'mdi:facebook',
+        url: 'https://www.facebook.com/cops.iitbhu',
+        content: 'Facebook Page',
+      },
+      {
+        icon: 'mdi:google',
+        url: 'https://groups.google.com/g/copsiitbhu',
+        content: 'Google Group',
+      },
+      {
+        icon: 'mdi:email',
+        url: 'mailto:copsiitbu@gmail.com',
+        content: 'Support Email ID',
+      },
+    ]
     return {
       logo: require('../assets/images/new-cops-logo.jpg'),
+      socialMedia,
     }
   },
 })
