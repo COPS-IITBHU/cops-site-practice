8469b259d32af9e7f98cd44aa6ce944ed747e612
P Aswini Kumar <aswini123p@gmail.com>
2021-05-30 02:04:58 +0530

    fixed linting error

--- components/About.vue
+++ components/About.vue
@@ -1,11 +1,20 @@
 <template>
-  <div class="tw-rounded-md tw-border-2 tw-border-gray-200 tw-shadow-md tw-bg-gray-50">
+  <div
+    class="
+      tw-rounded-md tw-border-2 tw-border-gray-200 tw-shadow-md tw-bg-gray-50
+    "
+  >
     <div class="tw-text-center tw-flex tw-justify-around">
-      <div class="tw-text-center tw-font-medium tw-text-3xl tw-my-2 tw-text-align-bottom">About us</div>
+      <div
+        class="
+          tw-text-center tw-font-medium tw-text-3xl tw-my-2 tw-text-align-bottom
+        "
+      >
+        About us
+      </div>
     </div>
     <div class="tw-text-center tw-mx-3 tw-px-2">
       <slot name="p1"></slot>
-      <hr class="tw-my-3" />
     </div>
   </div>
 </template>
