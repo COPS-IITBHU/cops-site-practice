32ef29a013f8907a3e7dfb9a2fe4d99746b27384
Anshuman <anshumanmaurya111@gmail.com>
2021-09-26 21:52:33 +0530

    added animated navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,170 +1,231 @@
 <template>
-  <nav app>
-    <v-app-bar hide-on-scroll elevtion-2 light app>
-      <v-app-bar-nav-icon
-        class="hidden-sm-and-up"
-        @click.native.stop="sideNav = !sideNav"
-      ></v-app-bar-nav-icon>
-      <v-toolbar-title text class="pl-0">
-        <span class="headline font-weight-bold" router exact to="/">COPS </span>
-        <span class="subheading font-weight-light">IIT(BHU)</span>
-      </v-toolbar-title>
-      <!-- <v-spacer></v-spacer> -->
-      <nav
-        class="
-          d-none d-sm-flex
-          justify-space-around
-          flex-grow-1 flex-lg-grow-0
-          ml-lg-auto
-        "
+  <div id="wrapper">
+    <button
+      id="toggle"
+      class="
+        navbar-toggle
+        tw-cursor-pointer tw-fixed tw-z-20 tw-top-4 tw-right-4 tw-p-0
+      "
+      style="background: transparent; border: none; outline: none"
+      type="button"
+      :class="{ active: isActive }"
+      @click="openNavbar"
+    >
+      <svg viewBox="0 0 100 100" width="80">
+        <path
+          class="line top"
+          d="m 30,33 h 40 c 0,0 9.044436,-0.654587 9.044436,-8.508902 0,-7.854315 -8.024349,-11.958003 -14.89975,-10.85914 -6.875401,1.098863 -13.637059,4.171617 -13.637059,16.368042 v 40"
+        />
+        <path class="line middle" d="m 30,50 h 40" />
+        <path
+          class="line bottom"
+          d="m 30,67 h 40 c 12.796276,0 15.357889,-11.717785 15.357889,-26.851538 0,-15.133752 -4.786586,-27.274118 -16.667516,-27.274118 -11.88093,0 -18.499247,6.994427 -18.435284,17.125656 l 0.252538,40"
+        />
+      </svg>
+    </button>
+    <div
+      class="navbar tw-top-0 tw-left-0 tw-w-screen tw-z-10 tw-fixed"
+      style="display: flex"
+    >
+      <div
+        v-for="div in 4"
+        :key="div"
+        class="navBg tw-h-0"
+        style="width: 25%; background-color: #7209b7"
+      ></div>
+      <h2
+        v-show="isOpen"
+        id="navTitle"
+        class="tw-top-4 tw-left-4 tw-opacity-0 tw-fixed tw-z-10 tw-text-white"
       >
-        <span v-for="item in links" :key="item.title">
-          <v-btn
-            :to="item.route ? item.route : ''"
-            depressed
-            router
-            exact
-            class="mx-md-2 mx-lg-3"
-          >
-            {{ item.title }}
-            <v-icon right>{{ item.icon }}</v-icon>
-          </v-btn>
-        </span>
-        <v-menu
-          v-for="item in multiLink"
-          :key="item.title"
-          transition="scale-transition"
-          open-on-hover
-          bottom
-          offset-y
-        >
-          <template v-slot:activator="{ on }">
-            <v-btn :to="item.route ? item.route : ''" depressed v-on="on">
-              {{ item.title }}
-              <v-icon small right>{{ item.icon }}</v-icon>
-            </v-btn>
-          </template>
-          <v-list>
-            <v-list-item
-              v-for="subroute in item.subroutes"
-              :key="subroute.title"
-              :to="subroute.route"
-              router
-              exact
-            >
-              <v-list-item-action>
-                <v-icon medium>{{ subroute.icon }}</v-icon>
-              </v-list-item-action>
-              <v-list-item-content class="subtitle-1">
-                {{ subroute.title }}
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-      </nav>
-    </v-app-bar>
-    <v-navigation-drawer v-model="sideNav" temporary app fixed light>
-      <v-list>
-        <v-list-item
-          v-for="item in links"
-          :key="item.title"
-          :to="item.route ? item.route : ''"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>{{ item.title }}</v-list-item-content>
-        </v-list-item>
-      </v-list>
-      <v-list two-line>
-        <template v-for="(item, index) in multiLink">
-          <v-subheader :key="item.title">
-            {{ item.title }}
-          </v-subheader>
-          <v-divider :key="index" />
-          <v-list-item
-            v-for="subroute in item.subroutes"
-            :key="subroute.title"
-            :to="subroute.route"
-            router
-            exact
-          >
-            <v-list-item-action>
-              <v-icon>{{ subroute.icon }}</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>{{ subroute.title }}</v-list-item-content>
-          </v-list-item>
-        </template>
-      </v-list>
-    </v-navigation-drawer>
-  </nav>
+        COPS - IIT(BHU)
+      </h2>
+      <ul
+        v-show="isOpen"
+        class="routeList tw-fixed tw-z-10 tw-flex tw-flex-col"
+      >
+        <nuxt-link to="/">
+          <li class="list_item" @click="closeNavbar" @keydown="closeNavbar">
+            Home
+          </li>
+        </nuxt-link>
+        <nuxt-link to="/blogs">
+          <li class="list_item" @click="closeNavbar" @keydown="closeNavbar">
+            Blogs
+          </li>
+        </nuxt-link>
+        <nuxt-link to="/group/cp">
+          <li class="list_item" @click="closeNavbar" @keydown="closeNavbar">
+            Groups
+          </li>
+        </nuxt-link>
+        <nuxt-link to="/csoc/resources">
+          <li class="list_item" @click="closeNavbar" @keydown="closeNavbar">
+            Resources
+          </li>
+        </nuxt-link>
+        <nuxt-link to="/csoc/timeline">
+          <li class="list_item" @click="closeNavbar" @keydown="closeNavbar">
+            Timeline
+          </li>
+        </nuxt-link>
+      </ul>
+    </div>
+  </div>
 </template>
 
-<script lang="ts">
-import {
-  mdiLaptopChromebook,
-  mdiBookshelf,
-  mdiMapClock,
-  mdiAccountGroup,
-  mdiChartLineVariant,
-  mdiGithub,
-  mdiCircleSlice3,
-  mdiHome,
-  mdiBlogger,
-} from '@mdi/js'
-
+<script>
 export default {
   data() {
     return {
-      sideNav: false,
-      links: [
-        { icon: mdiHome, title: 'Home', route: '/', hover: false },
-        { icon: mdiBlogger, title: 'Blog', route: '/blogs', hover: false },
-      ],
-      multiLink: [
-        {
-          icon: mdiLaptopChromebook,
-          title: 'CSOC',
-          route: '',
-          subroutes: [
-            {
-              icon: mdiBookshelf,
-              title: 'Resources',
-              route: '/csoc/resources',
-            },
-            {
-              icon: mdiMapClock,
-              title: 'Timeline',
-              route: '/csoc/timeline',
-            },
-          ],
-        },
-        {
-          icon: mdiAccountGroup,
-          title: 'Groups',
-          route: '',
-          subroutes: [
-            {
-              icon: mdiChartLineVariant,
-              title: 'CP',
-              route: '/group/cp',
-            },
-            {
-              icon: mdiGithub,
-              title: 'Dev',
-              route: '/group/dev',
-            },
-            {
-              icon: mdiCircleSlice3,
-              title: 'ML',
-              route: '/group/ml',
-            },
-          ],
-        },
-      ],
+      openTimeline: null,
+      isActive: false,
+      isOpen: false,
     }
+  },
+  mounted() {
+    this.configNavbar()
+  },
+  methods: {
+    configNavbar() {
+      this.openTimeline = this.$gsap
+        .timeline({ paused: true })
+        .to('.navBg', {
+          duration: 0.8,
+          height: '100vh',
+          stagger: 0.3,
+        })
+        .to('#navTitle', { duration: 0.6, opacity: 1 })
+        .to('.list_item', { duration: 0.6, y: 0, stagger: 0.15, opacity: 1 }, 1)
+    },
+    async openNavbar() {
+      if (this.isActive) {
+        this.isActive = false
+        await this.openTimeline.reverse().then(function () {
+          this.isOpen = false
+        })
+      } else {
+        this.openTimeline.play()
+        this.isActive = true
+        this.isOpen = true
+      }
+    },
+    async closeNavbar() {
+      this.isActive = false
+      await this.openTimeline.reverse().then(function () {
+        this.isOpen = false
+      })
+    },
   },
 }
 </script>
+
+<style scoped>
+/* BUTTON STYLING */
+.navbar-toggle {
+  -webkit-tap-highlight-color: transparent;
+  transition: transform 400ms;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+.navbar-toggle .line {
+  fill: none;
+  transition-delay: 400ms, 0;
+  transition-property: stroke, stroke-dasharray, stroke-dashoffset;
+  transition-timing-function: ease;
+  transition-duration: 400ms;
+  stroke: #000;
+  stroke-width: 5.5;
+  stroke-linecap: round;
+}
+.navbar-toggle .line.top {
+  stroke-dasharray: 40 139;
+}
+.navbar-toggle .line.bottom {
+  stroke-dasharray: 20 180;
+  stroke-dashoffset: -20px;
+}
+.navbar-toggle.active {
+  transform: rotate(45deg);
+}
+.navbar-toggle.active .line {
+  stroke: black;
+}
+.navbar-toggle.active .line.top {
+  stroke-dashoffset: -98px;
+}
+.navbar-toggle.active .line.bottom {
+  stroke-dashoffset: -138px;
+}
+.navbar-toggle:not(.active):hover .line.bottom {
+  stroke-dasharray: 40 180;
+  stroke-dashoffset: 0px;
+}
+/* NAVBAR STYLING */
+#navTitle {
+  font-size: 40px;
+  letter-spacing: 12px;
+  font-family: 'Roboto', 'Montserrat', sans-serif;
+  font-weight: 800;
+}
+.routeList {
+  list-style: none;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 250px;
+}
+.list_item {
+  position: relative;
+  font-size: 35px;
+  letter-spacing: 12px;
+  font-family: 'Montserrat', sans-serif;
+  cursor: pointer;
+  color: white;
+  text-align: center;
+  margin-bottom: 1rem;
+  transform: translate(0, 50%);
+  opacity: 0;
+  transition: letter-spacing 0.3s;
+}
+.list_item:hover {
+  letter-spacing: 15px;
+}
+a {
+  text-decoration: none;
+}
+.list_item:before {
+  content: ' ';
+  display: block;
+  position: absolute;
+  right: 0;
+  top: -2px;
+  width: 0;
+  height: 100%;
+  border-bottom: 2px solid white;
+  transition: 0.3s;
+}
+.list_item:hover:before {
+  width: 50%;
+}
+.list_item:after {
+  content: ' ';
+  display: block;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 0;
+  top: -2px;
+  height: 100%;
+  border-bottom: 2px solid white;
+  transition: 0.3s;
+}
+.list_item:hover:after {
+  width: 50%;
+}
+.selected {
+  border-bottom: 2px solid white;
+}
+</style>
